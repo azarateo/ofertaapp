de9a8b0c15f73f0e84a55b484b518362452be38e
azarateo <azarate@cool4code.com>
2014-07-31 17:51:18 -0500

    Initial Commit

---
+++ ofertaapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ofertaapp
+//
+//  Created by azarateo on 31/07/14.
+//  Copyright (c) 2014 COOL4CODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ofertaappAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ofertaappAppDelegate class]));
+    }
+}
---
+++ ofertaapp/ofertaappAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ofertaappAppDelegate.h
+//  ofertaapp
+//
+//  Created by azarateo on 31/07/14.
+//  Copyright (c) 2014 COOL4CODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ofertaappAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
